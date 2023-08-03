edaab21fb6087ea504d052d1d05611f22480fe47
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2023-08-04 04:28:29 +0530

    Fix some unhandled errors

--- ocpp/utils/model/model_enum.py
+++ ocpp/utils/model/model_enum.py
@@ -6,6 +6,11 @@ class ModelEnum(Enum):
     def choices(cls):
         return [(i.value, i.value) for i in cls]
 
+    @classmethod
+    def is_valid(cls, choice):
+        choices = {i.value for i in cls}
+        return choice in choices
+
     def __str__(self):
         return str(self.value)
 
